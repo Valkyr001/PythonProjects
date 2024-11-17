b5f8e645de11b01f03b0a71cb9afc10c50b95c23
Valkyr001 <bjhughes07@gmail.com>
2024-11-16 19:17:06 -0500

    updates

--- Ncrypt/ncrypt.py
+++ Ncrypt/ncrypt.py
@@ -8,9 +8,12 @@ from Crypto.Util.Padding import pad, unpad
 def parse():
     parser = argparse.ArgumentParser(description="Specify a file for encryption or decryption using a specific or master key")
 
-    parser.add_argument("mode", choices=["encrypt","decrypt"], help="Set mode for either encryption or decryption.")
-    parser.add_argument("-k", "--key", type=str, required=True, help="Specify the key or password used for encryption or decryption.")
-    parser.add_argument("-f", "--file", type=str, required=True, help="Specify the file path for the file to operate on.")
+    parser.add_argument("-e", "--encrypt", help="Encrypt the target file. -k and -f arguments are required.", action="store_true")
+    parser.add_argument("-d", "--decrypt", help="Decrypt the target file. -k and -f arguments required.", action="store_true")
+    parser.add_argument("-s", "--hashfile", help="Generate a SHA256 hash of the specified file.", action="store_true")
+    parser.add_argument("-k", "--key", type=str, help="Specify the key or password used for encryption or decryption.")
+    parser.add_argument("-f", "--file", type=str, help="Specify the full path of the target file.")
+
     return parser.parse_args()
 
 def deriveKey(string):
@@ -62,9 +65,7 @@ def decrypt():
         encryptedData = encrypted.read()
     
     cipher = AES.new(key, AES.MODE_CBC, iv)
-
-    ext = input("[i] Specify the file extension to output the decrypted file as (.txt, .webp, .png): ")
-
+    ext = input("\n[i] Specify the file extension to output the decrypted file as: ")
     try:
         decryptedData = unpad(cipher.decrypt(encryptedData), AES.block_size)
 
@@ -79,8 +80,46 @@ def decrypt():
     except ValueError as e:
         print("[!] Failed to decrypt file. Invalid key or corrupted data.")
 
+def hash():
+    args = parse()
+    file = args.file
+    try:
+        with open(file, "r") as f:
+            data = f.read()
+            sha256 = hashlib.sha256(data.encode()).hexdigest()
+            print(f"\n[+] SHA256: {sha256}")
+    except FileNotFoundError:
+        print("\n[!] Error specified file is invalid or does not exist.")
+
 args = parse()
-if args.mode == "encrypt":
-    encrypt()
-elif args.mode == "decrypt":
-    decrypt()
+if not args.hashfile and not args.encrypt and not args.decrypt:
+    print("\n[!] No valid operation specified. Must specify whether encrypting, decrypting, or hashing.")
+elif args.hashfile or args.encrypt or args.decrypt:
+    trueCount = 0
+    for i in (args.hashfile, args.encrypt, args.decrypt):
+        if i == True:
+            trueCount += 1
+    if trueCount > 1:
+        print("\n[!] Cannot specify more than one operation. (Encrypt, Decrypt, Hash)")
+    else:
+        if args.encrypt:
+            if not args.key == None:
+                if not args.file == None:
+                    encrypt()
+                else:
+                    print("\n[!] No valid target file specified.")
+            else:
+                print("\n[!] No key for encryption specified.")
+        elif args.decrypt:
+            if not args.key == None:
+                if not args.file == None:
+                    decrypt()
+                else:
+                    print("\n[!] No valid target file specified.")
+            else:
+                print("\n[!] No key for decryption specified.")
+        elif args.hashfile:
+            if not args.file == None:
+                hash()
+            else:
+                print("\n[!] No target file specified for hash operation.")
